b5a46b864a13a274f3e9256388acde4e1931d48f
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-12 17:39:32 +0000

    Add files via upload

---
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World and me");
+        }
+    }
+}
+
