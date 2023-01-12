0133997e85269eb805a9faf3f935f74434409cf3
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-12 17:41:17 +0000

    Update L1_3.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Lekcia 1/L1_3.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Lekcia 1/L1_3.cs
@@ -1,22 +1,22 @@
-﻿//using System;
+using System;
 
-//namespace Intro_and_Basic_Syntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            double grade = double.Parse(Console.ReadLine());
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
 
-//            if(grade >= 3.00)
-//            {
-//                Console.WriteLine("Passed!");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Failed!");
-//            }
-//        }
-//    }
-//}
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
 
