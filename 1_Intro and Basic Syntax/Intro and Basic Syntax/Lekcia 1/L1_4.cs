024aa16db0b7d8997e8bce25d597c5498623b3ae
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-12 17:41:50 +0000

    Update L1_4.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Lekcia 1/L1_4.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Lekcia 1/L1_4.cs
@@ -1,32 +1,32 @@
-﻿//using System;
+using System;
 
-//namespace Intro_and_Basic_Syntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int hours = int.Parse(Console.ReadLine());
-//            int min = int.Parse(Console.ReadLine());
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
 
-           
-//            int minwithAdditional = min + 30;
-//            int minmore = hours * 60;
-//            int minTotalAll = minwithAdditional + minmore;
-//            int hoursMore = minTotalAll / 60;
-//            int minTotalMore = minTotalAll % 60;
 
-            
+            int minwithAdditional = min + 30;
+            int minmore = hours * 60;
+            int minTotalAll = minwithAdditional + minmore;
+            int hoursMore = minTotalAll / 60;
+            int minTotalMore = minTotalAll % 60;
 
-//            if (minTotalMore < 10)
-//            {
-//                Console.WriteLine($"{hoursMore}:0{minTotalMore}");
-//            }
-//            else
-//            {
-//                Console.WriteLine($"{hoursMore}:{minTotalMore}");
-//            }
-//        }
-//    }
-//}
+
+
+            if (minTotalMore < 10)
+            {
+                Console.WriteLine($"{hoursMore}:0{minTotalMore}");
+            }
+            else
+            {
+                Console.WriteLine($"{hoursMore}:{minTotalMore}");
+            }
+        }
+    }
+}
 
