5385f632c9ba42812f8f9553b95dd225c30c9947
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-13 21:59:17 +0000

    Update 6_Foreign Languages.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Lekcia 1/6_Foreign Languages.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Lekcia 1/6_Foreign Languages.cs
@@ -1,1 +1,28 @@
+using System;
+
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country  = Console.ReadLine();
+
+            if(country == "USA" || country == "England")
+            {
+                Console.WriteLine("English");
+            }
+            else if(country == "Spain" || country == "Argentina" || country == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+                
+        }
+    }
+}
+
 
