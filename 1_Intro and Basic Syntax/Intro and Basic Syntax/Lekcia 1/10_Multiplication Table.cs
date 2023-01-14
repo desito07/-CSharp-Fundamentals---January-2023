e3b32a5f4d22d16253b286f2e73eb2ab856155e3
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-14 06:18:18 +0000

    Update and rename L1_10.cs to 10_Multiplication Table.cs

---
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Lekcia 1/10_Multiplication Table.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+           
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+            }
+        }
+    }
+}
+
