a0dd445e765864b607b9c4761cd27cc98917d2ba
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-15 22:26:02 +0000

    Update 01_Ages.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/01_Ages.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/01_Ages.cs
@@ -1,35 +1,37 @@
-﻿//using System;
+using System;
 
-//namespace Intro_and_Basic_Syntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-//            int num = int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
 
-//            if (num >= 0 && num <= 2)
-//            {
-//                Console.WriteLine("baby");
-//            }
-//            else if (num >= 3 && num <= 13)
-//            {
-//                Console.WriteLine("child");
-//            }
-//            else if (num >= 14 && num <= 19)
-//            {
-//                Console.WriteLine("teenager");
-//            }
-//            else if (num >= 20 && num <= 65)
-//            {
-//                Console.WriteLine("adult");
-//            }
-//            else if (num >= 66)
-//            {
-//                Console.WriteLine("elder");
-//            }
-//        }
-//    }
-//}
+            if (num >= 0 && num <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (num >= 3 && num <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (num >= 14 && num <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (num >= 20 && num <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (num >= 66)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
+
+
 
