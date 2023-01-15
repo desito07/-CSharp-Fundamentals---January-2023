9554367c09b3aa99e669fb2e4b6432e6c5b48e68
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-15 22:24:46 +0000

    Add files via upload

---
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/02_Division.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int division = 0;
+
+            if(num % 2 == 0 || num % 3 == 0 || num % 6 == 0)
+            {
+                division = 6;
+                Console.WriteLine($"The number is divisible by {division}");
+            }
+            else if(num % 2 == 0 || num % 10 == 0)
+            {
+                division = 10;
+                Console.WriteLine($"The number is divisible by {division}");
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}
+
---
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/06_Strong number.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+
+//namespace Intro_and_Basic_Syntax
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//        }
+//    }
+//}
+
