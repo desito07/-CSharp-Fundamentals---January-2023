2c14dbfc1a8ba37266ba3fc50c6495f183e22cc5
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-18 11:03:58 +0000

    Update 08_Triangle of Numbers.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/08_Triangle of Numbers.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/08_Triangle of Numbers.cs
@@ -1,14 +1,22 @@
-﻿//using System;
+using System;
 
-//namespace Intro_and_Basic_Syntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
 
-
-//        }
-//    }
-//}
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
 
