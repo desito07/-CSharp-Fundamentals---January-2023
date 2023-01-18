da6caccc511a0b808a1914ded71107ae8fec7812
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-18 22:53:19 +0000

    Update 11_Orders.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/11_Orders.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/11_Orders.cs
@@ -1,13 +1,27 @@
-﻿//using System;
+using System;
 
-//namespace Intro_and_Basic_Syntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-           
-//        }
-//    }
-//}
+namespace _11.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int orders = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            for (int i = 0; i < orders; i++)
+            {
+                double orderPrice = 0;
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsuleCnt = int.Parse(Console.ReadLine());
+                orderPrice = days * capsuleCnt * capsulePrice;
+                totalPrice += orderPrice;
+                Console.WriteLine($"The price for the coffee is: ${orderPrice:f2}");
+            }
 
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+
+        }
+    }
+}
