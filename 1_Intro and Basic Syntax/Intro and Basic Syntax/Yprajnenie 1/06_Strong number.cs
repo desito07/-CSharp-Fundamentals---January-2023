8bc26195f0c4256c4debc3086112f1383b744504
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-18 23:42:48 +0000

    Update 06_Strong number.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/06_Strong number.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/06_Strong number.cs
@@ -1,12 +1,38 @@
-﻿//using System;
+using System;
 
-//namespace Intro_and_Basic_Syntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//        }
-//    }
-//}
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int numbercopy = num;
+            int sum = 0;
+
+            while(numbercopy > 0)
+            {
+                int digit = numbercopy % 10;
+                numbercopy = numbercopy / 10;
+
+                int factorial = 1;
+
+                for (int i = 1; i <= digit; i++)
+                {
+                    factorial *= i;
+                }
+                sum += factorial;
+            }
+            if(sum == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }    
+        }
+    }
+}
 
