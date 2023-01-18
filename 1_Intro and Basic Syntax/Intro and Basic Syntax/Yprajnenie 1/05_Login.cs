7b40916716b3cbf40810a8876f1cd7fad2b04f0d
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-18 23:19:03 +0000

    Update 05_Login.cs

--- 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/05_Login.cs
+++ 1_Intro and Basic Syntax/Intro and Basic Syntax/Yprajnenie 1/05_Login.cs
@@ -1,13 +1,38 @@
-﻿//using System;
+using System;
 
-//namespace Intro_and_Basic_Syntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            
-//        }
-//    }
-//}
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                char currentChar = username[i];
+                password += username[i];
+
+            }
+            for (int i = 1; i <= 4; i++)
+            {
+                string current = Console.ReadLine();
+                if (current == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else if (i == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }
+        }
+    }
+}
 
