8d902c263ca501be62d5b00aed374620d3b33638
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-20 15:55:36 +0000

    Add files via upload

---
+++ 2_Data Types and Variables/Data Types and Variables Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data_Types_and_Variables_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
