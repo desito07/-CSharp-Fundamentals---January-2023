ac18d867b164e24379f560c215eeb8abc0ddd1a5
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-21 21:00:31 +0000

    Update 04_Centuries to Minutes.cs

--- 2_Data Types and Variables/Data Types and Variables Lab/04_Centuries to Minutes.cs
+++ 2_Data Types and Variables/Data Types and Variables Lab/04_Centuries to Minutes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Data_Types_and_Variables_Lab
 {
@@ -6,7 +6,12 @@ namespace Data_Types_and_Variables_Lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte centuries = byte.Parse(Console.ReadLine());
+            ushort years = (ushort)(centuries * 100);
+            uint days = (uint)(years * 365.2422);
+            ulong hours = days * 24;
+            double minutes = hours * 60;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
         }
     }
 }
