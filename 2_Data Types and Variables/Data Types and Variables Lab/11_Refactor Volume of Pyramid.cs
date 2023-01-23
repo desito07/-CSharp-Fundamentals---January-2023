a45b8d861fae9666400dfbf5f9619673cd64c6b8
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-23 16:24:25 +0000

    Update 11_Refactor Volume of Pyramid.cs

--- 2_Data Types and Variables/Data Types and Variables Lab/11_Refactor Volume of Pyramid.cs
+++ 2_Data Types and Variables/Data Types and Variables Lab/11_Refactor Volume of Pyramid.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Data_Types_and_Variables_Lab
 {
@@ -6,7 +6,26 @@ namespace Data_Types_and_Variables_Lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // the 'working code'
+
+            //double dul, sh, V = 0;
+            //Console.WriteLine("Length: ");
+            //dul = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Width: ");
+            //sh = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Heigth: ");
+            //V = double.Parse(Console.ReadLine());
+            //V = (dul + sh + V) / 3;
+            //Console.WriteLine($"Pyramid Volume: {V:f2}");
+
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double volume = (length * width * height) / 3;
+
+            Console.WriteLine($"Length: " + "Width: " + "Height: " + "Pyramid Volume: " + $"{volume:f2}");
         }
     }
 }
