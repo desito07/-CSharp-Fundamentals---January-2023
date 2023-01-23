4c58a79d7ae63fabc7e9f464d2ca68cf7e11dc33
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-23 16:23:22 +0000

    Update 10_Lower or Upper.cs

--- 2_Data Types and Variables/Data Types and Variables Lab/10_Lower or Upper.cs
+++ 2_Data Types and Variables/Data Types and Variables Lab/10_Lower or Upper.cs
@@ -6,15 +6,26 @@ namespace Data_Types_and_Variables_Lab
     {
         static void Main(string[] args)
         {
-            char ch = char.Parse(Console.ReadLine());
-            if(ch >= 65 && ch <= 90)
-            {
-                Console.WriteLine("upper-case");
-            }
-            else
-            {
-                Console.WriteLine("lower-case");
-            }
+
+            // the 'working code'
+
+            //double dul, sh, V = 0;
+            //Console.WriteLine("Length: ");
+            //dul = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Width: ");
+            //sh = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Heigth: ");
+            //V = double.Parse(Console.ReadLine());
+            //V = (dul + sh + V) / 3;
+            //Console.WriteLine($"Pyramid Volume: {V:f2}");
+
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double volume = (length * width * height) / 3;
+
+            Console.WriteLine($"Length: " + "Width: " + "Height: " + "Pyramid Volume: " + $"{volume:f2}");
         }
     }
 }
