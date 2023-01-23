af4740a3b53d5ed2988322294654d6d1bde9e01c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-23 16:54:28 +0000

    Add files via upload

---
+++ 2_Data Types and Variables/Data Types and Variables Exercise/04_Sum of Chars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data_Types_and_Variables_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
