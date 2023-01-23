796da6bc02a24ab9ef158ae8caa2528fe161399d
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-23 17:40:28 +0000

    Update 02_Sum Digits.cs

--- 2_Data Types and Variables/Data Types and Variables Exercise/02_Sum Digits.cs
+++ 2_Data Types and Variables/Data Types and Variables Exercise/02_Sum Digits.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Data_Types_and_Variables_Exercise
 {
@@ -6,7 +6,17 @@ namespace Data_Types_and_Variables_Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+            
+            int sum = 0;
+
+            while(num > 0)
+            {
+                sum += num % 10;
+                num = num / 10;
+            }
+            
+            Console.WriteLine(sum);
         }
     }
 }
