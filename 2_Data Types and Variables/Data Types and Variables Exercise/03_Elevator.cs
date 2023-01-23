c3ab12d70dc923b2a5a8f1aa54bf8a47427a1cca
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-23 22:58:12 +0000

    Update 03_Elevator.cs

--- 2_Data Types and Variables/Data Types and Variables Exercise/03_Elevator.cs
+++ 2_Data Types and Variables/Data Types and Variables Exercise/03_Elevator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Data_Types_and_Variables_Exercise
 {
@@ -6,7 +6,16 @@ namespace Data_Types_and_Variables_Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int course = numPeople / capacity;
+
+            if(numPeople % capacity != 0)
+            {
+                course++;
+            }
+            Console.WriteLine(course);
         }
     }
 }
