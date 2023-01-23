b3d4d14ccb7a57735766ba6a88819ca164202550
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-23 13:04:45 +0000

    Update 08_Town Info.cs

--- 2_Data Types and Variables/Data Types and Variables Lab/08_Town Info.cs
+++ 2_Data Types and Variables/Data Types and Variables Lab/08_Town Info.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Data_Types_and_Variables_Lab
 {
@@ -6,7 +6,11 @@ namespace Data_Types_and_Variables_Lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string townName = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            string area = Console.ReadLine();
+
+            Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
         }
     }
 }
