d1e6baa61d4aa7f5f8c9bec322aac68c262d9b60
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-23 11:54:23 +0000

    Update 05_Special Numbers.cs

--- 2_Data Types and Variables/Data Types and Variables Lab/05_Special Numbers.cs
+++ 2_Data Types and Variables/Data Types and Variables Lab/05_Special Numbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Data_Types_and_Variables_Lab
 {
@@ -6,7 +6,30 @@ namespace Data_Types_and_Variables_Lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 1; i <= num; i++)
+            {
+                int current = i; // sum of digits = 5 || 7 || 11
+                int sum = 0;
+
+                while (current > 0)
+                {
+                    sum += current % 10;
+
+                    current /= 10;
+                }
+                // if num is magic
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
         }
     }
 }
