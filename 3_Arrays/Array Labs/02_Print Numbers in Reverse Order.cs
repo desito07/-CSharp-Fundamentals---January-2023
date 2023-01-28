22b26d574d21cc9442525eb050835fb2332ace0b
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-28 23:10:07 +0000

    Update 02_Print Numbers in Reverse Order.cs

--- 3_Arrays/Array Labs/02_Print Numbers in Reverse Order.cs
+++ 3_Arrays/Array Labs/02_Print Numbers in Reverse Order.cs
@@ -1,13 +1,17 @@
-﻿//using System;
 
-//namespace Arrays_Lab
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+            int n = int.Parse(Console.ReadLine());
 
+            int[] array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                int current = int.Parse(Console.ReadLine());
+                array[i] = current;
+            }
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+       
