21c3f9e1707967434d4f023f05f806b198f219cf
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-29 23:21:36 +0000

    Update 06_Even and Odd Subtraction.cs

--- 3_Arrays/Array Labs/06_Even and Odd Subtraction.cs
+++ 3_Arrays/Array Labs/06_Even and Odd Subtraction.cs
@@ -1,13 +1,23 @@
-﻿//using System;
 
-//namespace Arrays_Lab
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
 
+int sumEven = 0;
+int sumOdd = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int current = numbers[i];
+    if (current % 2 == 0)
+    {
+        sumEven += current;
+    }
+    else
+    {
+        sumOdd += current;
+    }
+}
+int diff = sumEven - sumOdd;
+Console.WriteLine(diff);
