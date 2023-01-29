50997ade60c1e7e2e05b1ed8d2828de403ff0a6f
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-29 23:29:35 +0000

    Update 05_Sum Even Numbers.cs

--- 3_Arrays/Array Labs/05_Sum Even Numbers.cs
+++ 3_Arrays/Array Labs/05_Sum Even Numbers.cs
@@ -10,9 +10,18 @@ int sum = 0;
 for (int i = 0; i < numbers.Length; i++)
 {
     int current = numbers[i];
-    if(current % 2 == 0)
+    if (current % 2 == 0)
     {
         sum += current;
     }
 }
 Console.WriteLine(sum);
+
+//  Алтернатива
+
+//int numbers = Console.ReadLine()
+//    .Split(" ")
+//    .Select(int.Parse)
+//    .Where(x => x % 2 == 0)
+//    .Sum();
+//Console.WriteLine(numbers);
