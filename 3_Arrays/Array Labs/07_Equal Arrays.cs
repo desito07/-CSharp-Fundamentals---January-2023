c73f4b1162c7d0021a9dd7e3aa3598caf8a46625
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-29 23:53:40 +0000

    Update 07_Equal Arrays.cs

--- 3_Arrays/Array Labs/07_Equal Arrays.cs
+++ 3_Arrays/Array Labs/07_Equal Arrays.cs
@@ -1,13 +1,33 @@
-﻿//using System;
 
-//namespace Arrays_Lab
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+
+int[] num1 = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+
+
+int[] num2 = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int sum = 0;
+bool areSame = true;
+
+
+for (int i = 0; i < num1.Length; i++)
+{
+    sum += num1[i];
+    if (num1[i] != num2[i])
+    {
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        areSame = false;
+    break;
+    }
+}
+if (areSame)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+}
 
