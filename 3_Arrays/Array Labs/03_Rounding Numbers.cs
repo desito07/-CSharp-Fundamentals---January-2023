dabc8b7b396816251044dda6020faa0e48da1ccc
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-29 22:06:22 +0000

    Update 03_Rounding Numbers.cs

--- 3_Arrays/Array Labs/03_Rounding Numbers.cs
+++ 3_Arrays/Array Labs/03_Rounding Numbers.cs
@@ -6,12 +6,5 @@ double[] inputNumbers = Console.ReadLine()
 
 for (int i = 0; i < inputNumbers.Length; i++)
 {
-    if (inputNumbers[i] == -0)
-    {
-        Console.WriteLine($"{inputNumbers[i]} => 0");
-    }
-    else
-    {
-        Console.WriteLine($"{inputNumbers[i]} => {Math.Round(inputNumbers[i], MidpointRounding.AwayFromZero)}");
-    }
+        Console.WriteLine($"{inputNumbers[i]} => {(int)Math.Round(inputNumbers[i], MidpointRounding.AwayFromZero)}");
 }
