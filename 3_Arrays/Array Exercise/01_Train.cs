b79a7013ecff6dcf63683bb6a0146c2e4dab9ee7
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-30 22:49:10 +0000

    Add files via upload

---
+++ 3_Arrays/Array Exercise/01_Train.cs
@@ -0,0 +1,20 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[n];
+   
+int sum = 0; 
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int current = int.Parse(Console.ReadLine());
+    numbers[i] = current;
+    sum += current;
+    
+}
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine(sum);
+
+
+
+
