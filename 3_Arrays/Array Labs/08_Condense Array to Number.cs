fd44b6b3bf904ab93e470e1dc349cef577d5add2
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-30 22:04:01 +0000

    Update 08_Condense Array to Number.cs

--- 3_Arrays/Array Labs/08_Condense Array to Number.cs
+++ 3_Arrays/Array Labs/08_Condense Array to Number.cs
@@ -1,13 +1,22 @@
-﻿//using System;
+int[] inputNumbers = Console.ReadLine() // 10 20 30
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
 
-//namespace Arrays_Lab
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+//int[] condensedArray = new int[0];
 
+
+for (int i = 0; i < inputNumbers.Length - 1; i++)
+{
+
+    //condensedArray = inputNumbers; // 10 20 30
+
+    for (int j = 0; j < inputNumbers.Length - 1 - i; j++)
+    {
+        inputNumbers[j] = inputNumbers[j] + inputNumbers[j + 1];  
+    }
+
+    //inputNumbers = condensedArray;
+    
+}
+Console.WriteLine(inputNumbers[0]);
