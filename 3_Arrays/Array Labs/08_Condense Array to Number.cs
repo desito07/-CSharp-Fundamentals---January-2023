693025c76d61501a4020e4a7ac06285367ecd869
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-30 22:21:01 +0000

    Update 08_Condense Array to Number.cs

--- 3_Arrays/Array Labs/08_Condense Array to Number.cs
+++ 3_Arrays/Array Labs/08_Condense Array to Number.cs
@@ -3,20 +3,13 @@ int[] inputNumbers = Console.ReadLine() // 10 20 30
     .Select(int.Parse)
     .ToArray();
 
-//int[] condensedArray = new int[0];
-
+// 10 20 30 40 => 30 50 70 40 => 80 120 70 40 => 200 120 70 40 
 
 for (int i = 0; i < inputNumbers.Length - 1; i++)
 {
-
-    //condensedArray = inputNumbers; // 10 20 30
-
     for (int j = 0; j < inputNumbers.Length - 1 - i; j++)
     {
         inputNumbers[j] = inputNumbers[j] + inputNumbers[j + 1];  
     }
-
-    //inputNumbers = condensedArray;
-    
 }
 Console.WriteLine(inputNumbers[0]);
