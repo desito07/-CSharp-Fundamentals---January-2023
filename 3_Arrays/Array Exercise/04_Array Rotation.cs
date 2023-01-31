14e7787e0de4aaafff8030a165abba945413033c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-31 22:42:29 +0000

    Update 04_Array Rotation.cs

--- 3_Arrays/Array Exercise/04_Array Rotation.cs
+++ 3_Arrays/Array Exercise/04_Array Rotation.cs
@@ -1,2 +1,21 @@
 ﻿
+int[] inputNum = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+int rotation = int.Parse(Console.ReadLine());
 
+
+
+for (int j = 0; j < rotation; j++)
+{
+    int firstElement = inputNum[0];
+
+    for (int i = 0; i < inputNum.Length - 1; i++)
+    {
+        inputNum[i] = inputNum[i + 1];
+    }
+    inputNum[inputNum.Length - 1] = firstElement;
+}
+
+Console.WriteLine(string.Join(" ", inputNum));
