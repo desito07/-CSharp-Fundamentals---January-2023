18dbd4b5133ed9f3679d4c70738eff4c4e37e092
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-01 23:18:53 +0000

    Update 06_Equal Sum.cs

--- 3_Arrays/Array Exercise/06_Equal Sum.cs
+++ 3_Arrays/Array Exercise/06_Equal Sum.cs
@@ -1,3 +1,29 @@
+int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+
+int leftSum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum -= numbers[i];
+    if(leftSum == sum)
+    {
+        Console.WriteLine(i);
+        return;
+    }
+
+    leftSum += numbers[i];
+}
+Console.WriteLine("no");
+
 ﻿
 
 
