2dd1037488f32b5f8f95df80a27f21d85e56628a
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-02 23:03:38 +0000

    Update 07_Max Sequence of Equal Elements.cs

--- 3_Arrays/Array Exercise/07_Max Sequence of Equal Elements.cs
+++ 3_Arrays/Array Exercise/07_Max Sequence of Equal Elements.cs
@@ -1,1 +1,30 @@
-﻿
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int longestSequenceStartIndex = 0;
+int longestSequenceLength = 0;
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    int currentSequenceStartIndex = i - 1;
+    int currentSequesnceLength = 1;
+
+    while ( i < numbers.Length && numbers[i] == numbers[i - 1])
+    {
+        currentSequesnceLength++;
+        i++;
+    }
+
+    if(currentSequesnceLength > longestSequenceLength)
+    {
+        longestSequenceLength = currentSequesnceLength;
+        longestSequenceStartIndex = currentSequenceStartIndex;
+    }
+}
+
+for (int i = longestSequenceStartIndex; i < longestSequenceStartIndex + longestSequenceLength; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
