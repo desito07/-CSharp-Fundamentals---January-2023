9f112d57cd1cd671119d8301474dc4ac6629bbd7
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-06 21:24:44 +0000

    Update 01_Computer Store.cs

--- Mid Exam/01_Programming Fundamentals Mid Exam Retake/01_Mid Exam Tasks/01_Computer Store.cs
+++ Mid Exam/01_Programming Fundamentals Mid Exam Retake/01_Mid Exam Tasks/01_Computer Store.cs
@@ -1,53 +1,54 @@
-﻿//using System;
+using System;
 
-//namespace _01_Mid_Exam_Tasks
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            double sumNoTax = 0.0;
-//            string command = Console.ReadLine();
-           
+namespace _01_Mid_Exam_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumNoTax = 0.0;
+            string command = Console.ReadLine();
 
-//            while (command != "special" && command != "regular")
-//            {
-//                double current = double.Parse(command);
-//                if (current <= 0)
-//                    {
-//                        Console.WriteLine("Invalid price!");
-//                    }
-//                    else
-//                    {
-//                    sumNoTax += current;
-//                    }
 
-//                    command = Console.ReadLine();
-//                }
+            while (command != "special" && command != "regular")
+            {
+                double current = double.Parse(command);
+                if (current <= 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else
+                {
+                    sumNoTax += current;
+                }
 
-//            double tax = sumNoTax * 0.2;
-//            double totalSum = sumNoTax + tax;
+                command = Console.ReadLine();
+            }
 
-//            if (totalSum == 0)
-//            {
-//                Console.WriteLine("Invalid order!");
-//                return;
-//            }
+            double tax = sumNoTax * 0.2;
+            double totalSum = sumNoTax + tax;
 
-//            if (command == "special")
-//                {
-//                double discount = 0.1;
-//                totalSum -= totalSum * discount;
-//                }
-          
+            if (totalSum == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
 
-//            Console.WriteLine("Congratulations you've just bought a new computer!");
-//            Console.WriteLine($"Price without taxes: {sumNoTax:f2}$");
-//            Console.WriteLine($"Taxes: {tax:f2}$");
-//            Console.WriteLine("-----------");
-//            Console.WriteLine($"Total price: {totalSum:F2}$");
+            if (command == "special")
+            {
+                double discount = 0.1;
+                totalSum -= totalSum * discount;
+            }
 
-//        }
-//    }
-//}
+
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {sumNoTax:f2}$");
+            Console.WriteLine($"Taxes: {tax:f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {totalSum:F2}$");
+
+        }
+    }
+}
+
 
