7d59edc701b7589509d5604d74052ef29d924853
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-08 08:21:46 +0000

    Add files via upload

---
+++ Mid Exam/02_Programming Fundamentals Mid Exam Retake/02_Mid Exam Tasks/01_SoftUni Reception.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+
+//namespace _02_Mid_Exam_Tasks
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int empl1 = int.Parse(Console.ReadLine());
+//            int empl2 = int.Parse(Console.ReadLine());
+//            int empl3 = int.Parse(Console.ReadLine());
+
+//            int studentCount = int.Parse(Console.ReadLine());
+
+//            int efficiency = empl1 + empl2 + empl3;
+
+//            double time = 0;
+
+//            while (studentCount > 0)
+//            {
+//                time++;
+
+//                if (time % 4 == 0)
+//                {
+//                    time++;
+//                }
+//                studentCount -= efficiency;
+//            }
+//            Console.WriteLine($"Time needed: {time}h.");
+//        }
+//    }
+//}
+
---
+++ Mid Exam/02_Programming Fundamentals Mid Exam Retake/02_Mid Exam Tasks/02_Array Modifier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _02_Mid_Exam_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            
+
+
+            while(true)
+            {
+                string[] arr = Console.ReadLine().Split();
+
+                if (arr[0] == "end")
+                {
+                    break;
+                }
+                else if (arr[0] == "swap")
+                {
+                    int index1 = int.Parse(arr[1]);
+                    int index2 = int.Parse(arr[2]);
+                    int temp = num[index1];
+
+                    num[index1] = num[index2];
+                    num[index2] = temp;
+                }
+                else if (arr[0] == "multiply")
+                {
+                    int indexOne = int.Parse(arr[1]);
+                    int indexTwo = int.Parse(arr[2]);
+
+                    num[indexOne] = num[indexOne] * num[indexTwo];
+                }
+                else
+                {
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        num[i] = num[i] - 1;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", num));
+        }
+    }
+}
+
