c01380286d79e15e3641e620b595ec31152be918
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-08 08:22:43 +0000

    Update 01_SoftUni Reception.cs

--- Mid Exam/02_Programming Fundamentals Mid Exam Retake/02_Mid Exam Tasks/01_SoftUni Reception.cs
+++ Mid Exam/02_Programming Fundamentals Mid Exam Retake/02_Mid Exam Tasks/01_SoftUni Reception.cs
@@ -1,33 +1,33 @@
-﻿//using System;
+using System;
 
-//namespace _02_Mid_Exam_Tasks
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int empl1 = int.Parse(Console.ReadLine());
-//            int empl2 = int.Parse(Console.ReadLine());
-//            int empl3 = int.Parse(Console.ReadLine());
+namespace _02_Mid_Exam_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empl1 = int.Parse(Console.ReadLine());
+            int empl2 = int.Parse(Console.ReadLine());
+            int empl3 = int.Parse(Console.ReadLine());
 
-//            int studentCount = int.Parse(Console.ReadLine());
+            int studentCount = int.Parse(Console.ReadLine());
 
-//            int efficiency = empl1 + empl2 + empl3;
+            int efficiency = empl1 + empl2 + empl3;
 
-//            double time = 0;
+            double time = 0;
 
-//            while (studentCount > 0)
-//            {
-//                time++;
+            while (studentCount > 0)
+            {
+                time++;
 
-//                if (time % 4 == 0)
-//                {
-//                    time++;
-//                }
-//                studentCount -= efficiency;
-//            }
-//            Console.WriteLine($"Time needed: {time}h.");
-//        }
-//    }
-//}
+                if (time % 4 == 0)
+                {
+                    time++;
+                }
+                studentCount -= efficiency;
+            }
+            Console.WriteLine($"Time needed: {time}h.");
+        }
+    }
+}
 
