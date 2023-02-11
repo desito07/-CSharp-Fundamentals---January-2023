15f27a5175a6efa3ae782d639687e0fd97a577b1
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-11 20:46:48 +0000

    Add files via upload

---
+++ Mid Exam/04_Programming Fundamentals Mid Exam Retake/04_Mid Exam Tasks/02_Shopping List.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+
+//namespace _04_Mid_Exam_Tasks
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<string> goods = Console.ReadLine()
+//                .Split("!");
+
+//            while (true)
+//            {
+//                string[] arr = Console.ReadLine().Split();
+//                string command = arr[0];
+//                string product = arr[1];
+
+//                if (arr[0] == "Go Shopping!")
+//                {
+//                    break;
+//                }
+//                else if (arr[0] == "Urgent")
+//                {
+
+//                    if (goods[product])
+//                    {
+                        
+//                    }
+//                }
+//                else if (arr[0] == "Unnecessary")
+//                {
+
+//                }
+//                else if (arr[0] == "Correct")
+//                {
+
+//                }
+//                else if (arr[0] == "Rearrange")
+//                {
+
+//                }
+//            }
+//            Console.WriteLine(string.Join(", ", goods));
+//        }
+//    }
+//}
+
