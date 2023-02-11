62b14b05d4681b6da5ff91e63814d2cabe43953c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-11 21:40:14 +0000

    Update 02_Treasure Hunt.cs

--- Mid Exam/06_Programming Fundamentals Mid Exam Retake/06_Mid Exam Tasks/02_Treasure Hunt.cs
+++ Mid Exam/06_Programming Fundamentals Mid Exam Retake/06_Mid Exam Tasks/02_Treasure Hunt.cs
@@ -1,36 +1,36 @@
-﻿using System;
+// ﻿using System;
 
-namespace _06_Mid_Exam_Tasks
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string[] items = Console.ReadLine().Split("|");
+// namespace _06_Mid_Exam_Tasks
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             string[] items = Console.ReadLine().Split("|");
 
-            while (true)
-            {
-                string[] arr = Console.ReadLine().Split();
+//             while (true)
+//             {
+//                 string[] arr = Console.ReadLine().Split();
 
-                if (arr[0] == "Yohoho!")
-                {
-                    break;
-                }
-                else if(arr[0] == "Loot!")
-                {
+//                 if (arr[0] == "Yohoho!")
+//                 {
+//                     break;
+//                 }
+//                 else if(arr[0] == "Loot!")
+//                 {
 
-                }
-                else if (arr[0] == "Drop")
-                {
+//                 }
+//                 else if (arr[0] == "Drop")
+//                 {
 
-                }
-                else if (arr[0] == "Steal")
-                {
+//                 }
+//                 else if (arr[0] == "Steal")
+//                 {
 
-                }
+//                 }
 
-            }
-        }
-    }
-}
+//             }
+//         }
+//     }
+// }
 
