2cc1c9dd87c26fbe3c6503231c659135a6176ee2
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-14 23:26:01 +0000

    Update 02_Treasure Hunt.cs

--- Mid Exam/06_Programming Fundamentals Mid Exam Retake/06_Mid Exam Tasks/02_Treasure Hunt.cs
+++ Mid Exam/06_Programming Fundamentals Mid Exam Retake/06_Mid Exam Tasks/02_Treasure Hunt.cs
@@ -1,36 +1,90 @@
-// ﻿using System;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
-// namespace _06_Mid_Exam_Tasks
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             string[] items = Console.ReadLine().Split("|");
+namespace _06_Mid_Exam_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine()
+             .Split("|")
+             .ToList();
 
-//             while (true)
-//             {
-//                 string[] arr = Console.ReadLine().Split();
+            string command = Console.ReadLine();
 
-//                 if (arr[0] == "Yohoho!")
-//                 {
-//                     break;
-//                 }
-//                 else if(arr[0] == "Loot!")
-//                 {
+            while (true)
+            {
+                string[] commandInfo = command.Split();
 
-//                 }
-//                 else if (arr[0] == "Drop")
-//                 {
+                if (command == "Yohoho!")
+                {
+                    break;
+                }
+                else if (commandInfo[0] == "Loot")
+                {
+                    for (int i = 1; i < commandInfo.Length; i++)
+                    {
+                        if (!items.Contains(commandInfo[i]))
+                        {
+                            items.Insert(0, commandInfo[i]);
+                        }
+                    }  
+                }
+                else if (commandInfo[0] == "Drop")
+                {
+                    int index = int.Parse(commandInfo[1]);
 
-//                 }
-//                 else if (arr[0] == "Steal")
-//                 {
+                    if(index >= 0 && index < items.Count)
+                    {
+                        string currentElement = items[index];
+                        items.RemoveAt(index);
+                        items.Add(currentElement);
+                    }
+                }
+                else if (commandInfo[0] == "Steal")
+                {
+                    List<string> elements = new List<string>();
+                    int count = int.Parse(commandInfo[1]);
 
-//                 }
+                    while(count > 0 && items.Count > 0)
+                    {
+                        string currentElement = items[items.Count - 1];
+                        elements.Add(currentElement);
+                        items.RemoveAt(items.Count - 1);
 
-//             }
-//         }
-//     }
-// }
+
+                        count--;
+                    }
+                    elements.Reverse();
+
+                    Console.WriteLine(string.Join(", ", elements));
+                }
+
+                command = Console.ReadLine();
+
+            }
+            if(items.Count == 0)
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+            else
+            {
+                int sum = 0;
+
+                foreach (var item in items)
+                {
+                    sum += item.Length;
+                }
+
+                double result = sum / (double)items.Count;
+
+                Console.WriteLine($"Average treasure gain: {result:f2} pirate credits.");
+            }
+
+        }
+    }
+}
+
 
