cfdaa6ab41f3627e1ed48741e1c6a99a4f45fb7b
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-18 22:58:51 +0000

    Update 03_Moving Target.cs

--- Mid Exam/03_Programming Fundamentals Mid Exam Retake/03_Mid Exam Tasks/03_Moving Target.cs
+++ Mid Exam/03_Programming Fundamentals Mid Exam Retake/03_Mid Exam Tasks/03_Moving Target.cs
@@ -1,13 +1,64 @@
-﻿//using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace _03_Mid_Exam_Tasks
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+class ShootingGallery
+{
+    static void Main()
+    {
+        List<int> targets = Console.ReadLine().Split().Select(int.Parse).ToList();
 
+        string input = Console.ReadLine();
+        while (input != "End")
+        {
+            string[] command = input.Split();
+            string action = command[0];
+            int index = int.Parse(command[1]);
+
+            switch (action)
+            {
+                case "Shoot":
+                    int power = int.Parse(command[2]);
+                    if (index >= 0 && index < targets.Count)
+                    {
+                        targets[index] -= power;
+                        if (targets[index] <= 0)
+                        {
+                            targets.RemoveAt(index);
+                        }
+                    }
+                    break;
+
+                case "Add":
+                    int value = int.Parse(command[2]);
+                    if (index >= 0 && index < targets.Count)
+                    {
+                        targets.Insert(index, value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+                    break;
+
+                case "Strike":
+                    int radius = int.Parse(command[2]);
+                    int startIndex = index - radius;
+                    int endIndex = index + radius;
+                    if (startIndex >= 0 && endIndex < targets.Count)
+                    {
+                        targets.RemoveRange(startIndex, endIndex - startIndex + 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Strike missed!");
+                    }
+                    break;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine(string.Join("|", targets));
+    }
+}
