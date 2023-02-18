3118c769d7083775b0a4c2221730b6f18a45bd89
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-02-18 22:36:35 +0000

    Update 03_Numbers.cs

--- Mid Exam/02_Programming Fundamentals Mid Exam Retake/02_Mid Exam Tasks/03_Numbers.cs
+++ Mid Exam/02_Programming Fundamentals Mid Exam Retake/02_Mid Exam Tasks/03_Numbers.cs
@@ -1,13 +1,35 @@
-﻿//using System;
+using System;
+using System.Linq;
 
-//namespace _02_Mid_Exam_Tasks
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+
+namespace _02_Mid_Exam_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+            int average = (int)numbers.Average();
+
+            int[] result = numbers
+                .Where(c => c > average)
+                .OrderByDescending(c => c)
+                .Take(5)
+                .ToArray();
+
+            if(result.Length > 0)
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
 
